27ae856253ebdd338d94d9cd0bfa18960015c2d9
Franco <fmaggioni0@gmail.com>
2022-08-31 14:43:17 +0200

    fix HY360-105

--- src/lib/sort_features.js
+++ src/lib/sort_features.js
@@ -1,14 +1,16 @@
-import area from '@mapbox/geojson-area';
-import * as Constants from '../constants';
+import area from "@mapbox/geojson-area";
+import * as Constants from "../constants";
 
 const FEATURE_SORT_RANKS = {
   Point: 0,
   LineString: 1,
-  Polygon: 2
+  Polygon: 2,
 };
 
 function comparator(a, b) {
-  const score = FEATURE_SORT_RANKS[a.geometry.type] - FEATURE_SORT_RANKS[b.geometry.type];
+  const score =
+    FEATURE_SORT_RANKS[a.geometry.type.replace("Multi", "")] -
+    FEATURE_SORT_RANKS[b.geometry.type.replace("Multi", "")];
 
   if (score === 0 && a.geometry.type === Constants.geojsonTypes.POLYGON) {
     return a.area - b.area;
@@ -19,19 +21,25 @@ function comparator(a, b) {
 
 // Sort in the order above, then sort polygons by area ascending.
 function sortFeatures(features) {
-  return features.map((feature) => {
-    if (feature.geometry.type === Constants.geojsonTypes.POLYGON) {
-      feature.area = area.geometry({
-        type: Constants.geojsonTypes.FEATURE,
-        property: {},
-        geometry: feature.geometry
-      });
-    }
-    return feature;
-  }).sort(comparator).map((feature) => {
-    delete feature.area;
-    return feature;
-  });
+  return features
+    .map((feature) => {
+      if (
+        feature.geometry.type === Constants.geojsonTypes.POLYGON ||
+        feature.geometry.type === Constants.geojsonTypes.MULTI_POLYGON
+      ) {
+        feature.area = area.geometry({
+          type: Constants.geojsonTypes.FEATURE,
+          property: {},
+          geometry: feature.geometry,
+        });
+      }
+      return feature;
+    })
+    .sort(comparator)
+    .map((feature) => {
+      delete feature.area;
+      return feature;
+    });
 }
 
 export default sortFeatures;
